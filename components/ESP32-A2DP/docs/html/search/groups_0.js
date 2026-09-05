9b1eb9cddac73b239466f4007d3c8fdaa97dd0e0
sprlightning <18274618560@163.com>
2026-09-06 07:53:12 +0800

    Remove Arduino A2DP stack and legacy main
    Drop components/ESP32-A2DP and components/arduino-audio-tools and the
    Arduino-style main.cpp; the project moves to a native ESP-IDF BT stack.

--- components/ESP32-A2DP/docs/html/search/groups_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['esp32_20a2dp_362',['ESP32 A2DP',['../group__a2dp.html',1,'']]]
-];
--- components/arduino-audio-tools/src/AudioTools/CoreAudio/AudioMetaData.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "AudioTools/CoreAudio/AudioMetaData/MetaData.h"
-#include "AudioTools/CoreAudio/AudioMetaData/MimeDetector.h"
-#include "AudioTools/CoreAudio/AudioMetaData/MimeResolver.h"
